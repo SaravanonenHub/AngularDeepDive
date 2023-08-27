83f7321a3bf563e51cf68105375322d5efe77be3
EXTERNAL Nanjundan Saravanan (Ancit <naj2cob@bosch.com>
2023-08-27 17:38:04 +0530

    Lifecycle part ii

--- angular-deep/src/app/component-life-cycle/component-life-cycle-routing.module.ts
+++ angular-deep/src/app/component-life-cycle/component-life-cycle-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SpyComponent } from './spy.component';
 import { OnChangesParentComponent } from './on-changes-parent.component';
+import { AfterViewComponent } from './after-view.component';
+import { AfterViewParentComponent } from './after-view-parent.component';
+import { AfterContentParentComponent } from './after-content-parent.component';
+import { DoCheckParentComponent } from './do-check-parent.component';
 
 const routes: Routes = [
   {path:'spy',component:SpyComponent}
   ,{path:'onChange',component:OnChangesParentComponent}
+  ,{path:'AfterViewChange',component:AfterViewParentComponent}
+  ,{path:'AfterContentChange',component:AfterContentParentComponent}
+  ,{path:'doCheck',component:DoCheckParentComponent}
 ];
 
 @NgModule({
--- angular-deep/src/app/component-life-cycle/component-life-cycle.module.ts
+++ angular-deep/src/app/component-life-cycle/component-life-cycle.module.ts
@@ -7,13 +7,27 @@ import { SpyComponent } from './spy.component';
 import { FormsModule } from '@angular/forms';
 import { OnChangesParentComponent } from './on-changes-parent.component';
 import { OnChangesComponent } from './on-changes.component';
+import { ChildViewsComponent } from './child-views.component';
+import { AfterViewComponent } from './after-view.component';
+import { AfterViewParentComponent } from './after-view-parent.component';
+import { AfterContentParentComponent } from './after-content-parent.component';
+import { AfterContentComponent } from './after-content.component';
+import { DoCheckComponent } from './do-check.component';
+import { DoCheckParentComponent } from './do-check-parent.component';
 
 @NgModule({
   declarations: [
     SpyDirective,
     SpyComponent,
     OnChangesParentComponent,
-    OnChangesComponent
+    OnChangesComponent,
+    ChildViewsComponent,
+    AfterViewComponent,
+    AfterViewParentComponent,
+    AfterContentParentComponent,
+    AfterContentComponent,
+    DoCheckComponent,
+    DoCheckParentComponent
   ],
   imports: [
     CommonModule,
